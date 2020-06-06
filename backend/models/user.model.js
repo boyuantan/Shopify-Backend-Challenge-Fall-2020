9ba4282471c84c2388b9bd405b58cf5c7ed2cf5c
boyuantan <boyuantan0@gmail.com>
2020-06-06 16:47:15 -0400

    add secure password storage and matching

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
   username: {
@@ -9,10 +12,34 @@ const userSchema = new Schema({
     trim: true,
     minlength: 3,
   },
-}, {
-  timestamps: true,
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
 
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+    // Hash password with new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+// Exports
+const User = mongoose.model('User', userSchema);
 module.exports = User;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,19 +1,59 @@
+const crypto = require('crypto');
 const router = require('express').Router();
+
 let User = require('../models/user.model');
 
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+// Use random character string as salt
+var genSalt = function(length) {
+  return crypto.randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
+}
 
-router.route('/add').post((req, res) => {
+// Using SHA512
+var sha512 = function(password, salt) {
+  var hash = crypto.createHmac('sha512', salt);
+  hash.update(password);
+  var value = hash.digest('hex');
+  return {
+    salt: salt,
+    pwdHash: value,
+  }
+}
+
+function genSaltedHash(password) {
+  var salt = getRandomString(16);
+  var passwordData = sha512(password, salt);
+}
+
+// router.route('/').get((req, res) => {
+//   User.find()
+//     .then(users => res.json(users))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+router.route('/register').post((req, res) => {
   const username = req.body.username;
-  const newUser = new User({username});
+  const password = req.body.password;
+  const newUser = new User({username, password});
 
   newUser.save()
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/login').post((req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username: username }, (err, user) => {
+    if (err) throw err;
+
+    user.comparePassword(password, (err, isMatch) => {
+      if (err) throw err;
+      console.log(password, ': ', isMatch);
+    });
+  });
 });
 
 module.exports = router;
